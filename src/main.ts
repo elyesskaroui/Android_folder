39d396602a9bd0280523ecdc91a8702607eec791
elyessKaroui <elyess.karoui@esprit.tn>
2025-03-12 13:57:51 +0100

    main fix

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document); // Swagger UI available at /api
 
   // Start the server
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3001;
   await app.listen(PORT);
   console.log(`Application is running on: http://localhost:${PORT}`);
 }
